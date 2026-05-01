304695c7fa929ca2d66e0e4ebe120590a3c4a502
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-01 17:45:19 -0400

    feat: cria entidade Personagem

---
+++ models/Personagem.cs
@@ -0,0 +1,9 @@
+namespace ProjectCSharp.models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+    }
+}
