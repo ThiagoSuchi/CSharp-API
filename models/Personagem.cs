40ca5dc4c688a66f0e03d25b454931dc06bbffa9
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-04 16:43:42 -0400

    feat: adicionar validações e Data Annotations ao modelo Personagem

--- models/Personagem.cs
+++ models/Personagem.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ProjectCSharp.models
 {
     public class Personagem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Garante a criação como AUTO_INCREMENT no MySQL
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome deve ser obrigatório")]
+        [MaxLength(50, ErrorMessage = "Nome precisa ter no máximo 50 caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Categoria deve ser obrigatório")]
+        [MaxLength(15, ErrorMessage = "Deve conter no máximo 15 caracteres")]
         public string Categoria { get; set; }
     }
 }
